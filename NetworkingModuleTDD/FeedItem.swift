87df2ae719d58308bea3e63aa84f5afb899e564a
Shashank Mishra <mishra.shashank35@gmail.com>
2021-10-15 17:29:34 +0530

    Intial code

---
+++ NetworkingModuleTDD/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  NetworkingModuleTDD
+//
+//  Created by Shashank Mishra on 15/10/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}
