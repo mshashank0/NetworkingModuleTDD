7dabae6d55ffc97ac67d82c4b8e40df5570ed5d8
Shashank Mishra <mishra.shashank35@gmail.com>
2021-10-15 19:25:41 +0530

    Pass url instead of hardcoding

--- NetworkingModuleTDDTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTDDTests/RemoteFeedLoaderTests.swift
@@ -10,11 +10,13 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
-    init(client: HTTPClient) {
+    let url: URL
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
-    func load() {
-        client.get(from: URL(string: "http://exmaple.com/xyz")!)
+    func load(url: URL) {
+        client.get(from: url)
     }
 }
 
@@ -32,16 +34,18 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://xyz.com.v1")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "http://xyz.com.v1")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
-        sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        let sut = RemoteFeedLoader(url: url, client: client)
+        sut.load(url: url)
+        XCTAssertEqual(client.requestedURL, url)
     }
 
 }
