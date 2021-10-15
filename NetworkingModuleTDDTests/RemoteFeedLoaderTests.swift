9dd5bb6f980f0a2817a642fca29c2368aee3f27e
Shashank Mishra <mishra.shashank35@gmail.com>
2021-10-15 19:30:56 +0530

    Separate SUT method - refactor

--- NetworkingModuleTDDTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTDDTests/RemoteFeedLoaderTests.swift
@@ -24,28 +24,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        self.requestedURL = url
-    }
-    var requestedURL: URL?
-}
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let url = URL(string: "http://xyz.com.v1")!
-        let client = HTTPClientSpy()
+        let (_, client) = makeSUT(url: url)
         _ = RemoteFeedLoader(url: url, client: client)
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://xyz.com.v1")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         sut.load(url: url)
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "http://xyz.com.v1")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            self.requestedURL = url
+        }
     }
 
 }
