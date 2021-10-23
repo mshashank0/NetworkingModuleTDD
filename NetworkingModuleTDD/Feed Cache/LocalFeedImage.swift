ae45bdea85f26b162e69e1b64ecfe62a7b71d188
Shashank Mishra <mishra.shashank35@gmail.com>
2021-10-23 22:37:31 +0530

    Refactoring

--- NetworkingModuleTDD/Feed Cache/LocalFeedImage.swift
+++ NetworkingModuleTDD/Feed Cache/LocalFeedImage.swift
@@ -1,22 +1,21 @@
 //
-//  FeedItem.swift
+//  LocalFeedImage.swift
 //  NetworkingModuleTDD
 //
-//  Created by Shashank Mishra on 15/10/21.
+//  Created by Shashank Mishra on 23/10/21.
 //
-
 import Foundation
 
-public struct FeedItem: Equatable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
-    public let imageURL: URL
+    public let url: URL
     
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = imageURL
+        self.url = url
     }
 }
---
+++ NetworkingModuleTDD/Feed Feature/FeedImage.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImage.swift
+//  NetworkingModuleTDD
+//
+//  Created by Shashank Mishra on 15/10/21.
+//
+
+import Foundation
+
+public struct FeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+    
+    public init(id: UUID, description: String?, location: String?, url: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = url
+    }
+}
